13c7e3c33cab552f9c139b708051e46d096b35ab
cyc60 <avsysoev60@gmail.com>
2020-03-26 19:50:48 +0300

    Clean up utils

--- datastore/utils.go
+++ datastore/utils.go
@@ -2,7 +2,6 @@ package datastore
 
 import (
 	"crypto/rand"
-	"io"
 
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 )
@@ -25,17 +24,3 @@ func generateKey(passphrase string) (*Key, error) {
 		KeyFile: string(json),
 	}, nil
 }
-
-func encodeToString(max int) string {
-	b := make([]byte, max)
-	n, err := io.ReadAtLeast(rand.Reader, b, max)
-	if n != max {
-		panic(err)
-	}
-	for i := 0; i < len(b); i++ {
-		b[i] = table[int(b[i])%len(table)]
-	}
-	return string(b)
-}
-
-var table = [...]byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
